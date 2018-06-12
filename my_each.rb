411547d2e9244ec4639430a1d2097bfb2c3e3f85
Cristina Lugo <cmlugoce@syr.edu>
2018-06-12 17:43:28 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
-def my_each (array)
-  collection = [1, 2, 3, 4]
-  
-  if block_given?
-    i = 0
-  my_each(collection) do |i|
-    puts i 
-end 
+ef my_each(array)
+  i = 0
+  while i < array.length
+    yield array[i]
+    i += 1
+  end
+  array
+end
